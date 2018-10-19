ddf64c801eb7c891bb8cda047c59c7ee4ec60cc8
jef-rl <jef.richards@restaurant-live.co.uk>
2018-10-19 05:12:28 +0100

    Oct2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Meta, Title } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MarkdownModule } from 'ngx-markdown';
+import { AppSharedModule } from './_shared/app.shared.module';
+import { PipesModule } from './_shared/pipes/pipes.module';
+import { UserService } from './_shared/services/user.service';
 import { AppComponent } from './app.component';
+import { AppMaterialModule } from './app.material.module';
+import { AppRoutingModule } from './app.routing';
+import { SearchService } from './_shared/services/search.service';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppMaterialModule,
+    MarkdownModule.forRoot(),
+    PipesModule,
+    AppSharedModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [SearchService, UserService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  functionsURL: 'https://us-central1-spa-voucher.cloudfunctions.net',
+  firebase: {
+    apiKey: 'AIzaSyBbiCqbqJwjEiinoCrgfkE9zurR2L0DTwU',
+    authDomain: 'spa-voucher.firebaseapp.com',
+    databaseURL: 'https://spa-voucher.firebaseio.com',
+    projectId: 'spa-voucher',
+    storageBucket: 'spa-voucher.appspot.com',
+  }
 };
 
 /*
